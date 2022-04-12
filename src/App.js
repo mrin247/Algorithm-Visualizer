6aa22ebe7d623ff3fec0282859fc90645d486987
Mrin <mrinmoym247@gmail.com>
2022-04-12 13:54:00 +0530

    Algorithm Visualizer version-1:[Complete]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { HashRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import Pathfinder from "./pathfinderComponents/pathfinder";
+import Home from "./homeComponents/home";
+import Seive from "./primeComponents/seive";
+import Sort from "./sortComponents/sort";
+import Queen from "./queenComponents/queen";
+import ConvexHull from "./convexHullComponents/convexHull";
+import BinarySearch from "./binarySearchComponent/binarySearch";
+import RecursiveSort from "./recursiveSortComponents/recursiveSort";
+class App extends Component {
+  constructor() {
+    super();
+  }
+  componentDidMount() {
+    console.log(window.innerHeight, "  ", window.innerWidth);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      // <Router basename='/'>
+      //     <Switch>
+      //         <Route path='/pathfinder'  element={Pathfinder}/>
+      //         <Route path='/prime' element={Seive}/>
+      //         <Route path='/sort' element={Sort}/>
+      //         <Route path='/nqueen' element={Queen}/>
+      //         <Route path='/convexhull' element={ConvexHull}/>
+      //         <Route path='/binarysearch' element={BinarySearch}/>
+      //         <Route path='/recursivesort' element={RecursiveSort}/>
+      //         <Route path='/' element={Home}/>
+      //     </Switch>
+      // </Router>
+
+      <Router>
+        <Fragment>
+          <Routes>
+            <Route path="/pathfinder" element={<Pathfinder/>} />
+            <Route path="/prime" element={<Seive/>} />
+            <Route path="/sort" element={<Sort/>} />
+            <Route path="/nqueen" element={<Queen/>} />
+            <Route path="/convexhull" element={<ConvexHull/>} />
+            <Route path="/binarysearch" element={<BinarySearch/>} />
+            <Route path="/recursivesort" element={<RecursiveSort/>} />
+            <Route path="/" element={<Home/>} />
+          </Routes>
+        </Fragment>
+      </Router>
+    );
+  }
 }
 
 export default App;
